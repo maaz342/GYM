2daa1182846a31b5be72b6e86f8849c487795ce2
chames@gmail.com <maazkamran72@gmail.com>
2023-06-07 11:47:09 +0500

    important

--- WindowsFormsApp5/Addmember.Designer.cs
+++ WindowsFormsApp5/Addmember.Designer.cs
@@ -28,12 +28,258 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NUMBER = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Gender = new System.Windows.Forms.ComboBox();
+            this.Amount = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.timing = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.add = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(354, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "FITNESS CENTER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(320, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ADD NEW MEMBER";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Member Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 145);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // NUMBER
+            // 
+            this.NUMBER.AutoSize = true;
+            this.NUMBER.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.NUMBER.ForeColor = System.Drawing.Color.Navy;
+            this.NUMBER.Location = new System.Drawing.Point(336, 118);
+            this.NUMBER.Name = "NUMBER";
+            this.NUMBER.Size = new System.Drawing.Size(157, 23);
+            this.NUMBER.TabIndex = 6;
+            this.NUMBER.Text = "Phone Number";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(340, 145);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(661, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Age";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(665, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(269, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label6.ForeColor = System.Drawing.Color.Navy;
+            this.label6.Location = new System.Drawing.Point(12, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Gender";
+            // 
+            // Gender
+            // 
+            this.Gender.Font = new System.Drawing.Font("Century Gothic", 14F);
+            this.Gender.FormattingEnabled = true;
+            this.Gender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.Gender.Location = new System.Drawing.Point(16, 243);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(121, 35);
+            this.Gender.TabIndex = 11;
+            // 
+            // Amount
+            // 
+            this.Amount.AutoSize = true;
+            this.Amount.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.Amount.ForeColor = System.Drawing.Color.Navy;
+            this.Amount.Location = new System.Drawing.Point(336, 219);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(171, 23);
+            this.Amount.TabIndex = 12;
+            this.Amount.Text = "Monthly Amount";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(340, 245);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(269, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // timing
+            // 
+            this.timing.AutoSize = true;
+            this.timing.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.timing.ForeColor = System.Drawing.Color.Navy;
+            this.timing.Location = new System.Drawing.Point(661, 219);
+            this.timing.Name = "timing";
+            this.timing.Size = new System.Drawing.Size(68, 23);
+            this.timing.TabIndex = 14;
+            this.timing.Text = "Timing";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 14F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "6-8",
+            "9-11",
+            "1-4",
+            "6-10"});
+            this.comboBox1.Location = new System.Drawing.Point(665, 254);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 35);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.add.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.add.Location = new System.Drawing.Point(155, 357);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(103, 39);
+            this.add.TabIndex = 16;
+            this.add.Text = "ADD";
+            this.add.UseVisualStyleBackColor = false;
+            this.add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(611, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(842, 373);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Addmember
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(979, 515);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.timing);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.NUMBER);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Addmember";
             this.Text = "Addmember";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label NUMBER;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox Gender;
+        private System.Windows.Forms.Label Amount;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label timing;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
