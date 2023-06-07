5454f4856e00e22f8cbc3aadc9754022b6a35b07
chames@gmail.com <maazkamran72@gmail.com>
2023-06-07 13:02:03 +0500

    imporant

--- WindowsFormsApp5/Addmember.Designer.cs
+++ WindowsFormsApp5/Addmember.Designer.cs
@@ -43,9 +43,9 @@
             this.timing = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.add = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -75,65 +75,72 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.label3.ForeColor = System.Drawing.Color.Navy;
-            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Location = new System.Drawing.Point(55, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(159, 23);
             this.label3.TabIndex = 4;
             this.label3.Text = "Member Name";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 145);
+            this.textBox1.Location = new System.Drawing.Point(59, 158);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(269, 22);
             this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // NUMBER
             // 
             this.NUMBER.AutoSize = true;
             this.NUMBER.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.NUMBER.ForeColor = System.Drawing.Color.Navy;
-            this.NUMBER.Location = new System.Drawing.Point(336, 118);
+            this.NUMBER.Location = new System.Drawing.Point(57, 183);
             this.NUMBER.Name = "NUMBER";
             this.NUMBER.Size = new System.Drawing.Size(157, 23);
             this.NUMBER.TabIndex = 6;
             this.NUMBER.Text = "Phone Number";
+            this.NUMBER.Click += new System.EventHandler(this.NUMBER_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(340, 145);
+            this.textBox2.Location = new System.Drawing.Point(59, 219);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(269, 22);
             this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.label5.ForeColor = System.Drawing.Color.Navy;
-            this.label5.Location = new System.Drawing.Point(661, 119);
+            this.label5.Location = new System.Drawing.Point(57, 244);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(51, 23);
             this.label5.TabIndex = 8;
             this.label5.Text = "Age";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(665, 145);
+            this.textBox3.Location = new System.Drawing.Point(59, 277);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(269, 22);
             this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.label6.ForeColor = System.Drawing.Color.Navy;
-            this.label6.Location = new System.Drawing.Point(12, 216);
+            this.label6.Location = new System.Drawing.Point(57, 418);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(85, 23);
             this.label6.TabIndex = 10;
             this.label6.Text = "Gender";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // Gender
             // 
@@ -142,39 +149,43 @@
             this.Gender.Items.AddRange(new object[] {
             "Male",
             "Female"});
-            this.Gender.Location = new System.Drawing.Point(16, 243);
+            this.Gender.Location = new System.Drawing.Point(61, 447);
             this.Gender.Name = "Gender";
             this.Gender.Size = new System.Drawing.Size(121, 35);
             this.Gender.TabIndex = 11;
+            this.Gender.SelectedIndexChanged += new System.EventHandler(this.Gender_SelectedIndexChanged);
             // 
             // Amount
             // 
             this.Amount.AutoSize = true;
             this.Amount.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.Amount.ForeColor = System.Drawing.Color.Navy;
-            this.Amount.Location = new System.Drawing.Point(336, 219);
+            this.Amount.Location = new System.Drawing.Point(55, 303);
             this.Amount.Name = "Amount";
             this.Amount.Size = new System.Drawing.Size(171, 23);
             this.Amount.TabIndex = 12;
             this.Amount.Text = "Monthly Amount";
+            this.Amount.Click += new System.EventHandler(this.Amount_Click);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(340, 245);
+            this.textBox4.Location = new System.Drawing.Point(61, 329);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(269, 22);
             this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // timing
             // 
             this.timing.AutoSize = true;
             this.timing.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.timing.ForeColor = System.Drawing.Color.Navy;
-            this.timing.Location = new System.Drawing.Point(661, 219);
+            this.timing.Location = new System.Drawing.Point(57, 354);
             this.timing.Name = "timing";
             this.timing.Size = new System.Drawing.Size(68, 23);
             this.timing.TabIndex = 14;
             this.timing.Text = "Timing";
+            this.timing.Click += new System.EventHandler(this.timing_Click);
             // 
             // comboBox1
             // 
@@ -185,60 +196,72 @@
             "9-11",
             "1-4",
             "6-10"});
-            this.comboBox1.Location = new System.Drawing.Point(665, 254);
+            this.comboBox1.Location = new System.Drawing.Point(59, 380);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 35);
             this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // add
             // 
             this.add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.add.Font = new System.Drawing.Font("Century Gothic", 12F);
             this.add.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.add.Location = new System.Drawing.Point(155, 357);
+            this.add.Location = new System.Drawing.Point(22, 501);
             this.add.Name = "add";
             this.add.Size = new System.Drawing.Size(103, 39);
             this.add.TabIndex = 16;
-            this.add.Text = "ADD";
+            this.add.Text = "Update";
             this.add.UseVisualStyleBackColor = false;
             this.add.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(111, 566);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 39);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(387, 373);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(549, 551);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Size = new System.Drawing.Size(103, 39);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "BACK";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(611, 373);
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(168, 501);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 18;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(842, 373);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 19;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button3.Size = new System.Drawing.Size(103, 39);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "BACK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Addmember
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(979, 515);
-            this.Controls.Add(this.button4);
+            this.ClientSize = new System.Drawing.Size(979, 602);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.add);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.timing);
@@ -256,6 +279,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Addmember";
             this.Text = "Addmember";
+            this.Load += new System.EventHandler(this.Addmember_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -278,8 +302,8 @@
         private System.Windows.Forms.Label timing;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
--- WindowsFormsApp5/Addmember.cs
+++ WindowsFormsApp5/Addmember.cs
@@ -21,5 +21,80 @@ namespace WindowsFormsApp5
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NUMBER_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Addmember_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timing_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Amount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
